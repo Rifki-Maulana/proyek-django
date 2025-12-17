20750623ab31da241f47ebb24ef6accccf4fd760
A710230074 <a710230074@example.com>
2025-12-17 21:41:36 +0700

    penambahan fitur autentikasi

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', blog_views.custom_logout, name='logout'),
     path('', include('blog.urls')), 
 ]
